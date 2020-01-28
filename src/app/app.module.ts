52cd713d3fb017e6513d24814b4e4d2cfa7770a6
tw1ttt3r <tw1ttt3r@gmail.com>
2020-01-27 23:02:00 -0600

    feat: imported angular/fire modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,13 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { AngularFireModule } from '@angular/fire';
 import { AppRoutingModule } from './app-routing.module';
+import { AngularFirestoreModule } from '@angular/fire/firestore';
+
 import { AppComponent } from './app.component';
 import { ListsongsComponent } from './components/listsongs/listsongs.component';
 import { AddsongComponent } from './components/addsong/addsong.component';
+import { environment } from 'src/environments/environment';
 
 @NgModule({
   declarations: [
@@ -14,7 +17,9 @@ import { AddsongComponent } from './components/addsong/addsong.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AngularFireModule.initializeApp(environment.firebase),
+    AngularFirestoreModule, // imports firebase/firestore, only needed for database features
   ],
   providers: [],
   bootstrap: [AppComponent]

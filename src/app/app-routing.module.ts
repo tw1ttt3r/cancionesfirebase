7df38af5c942ad9f24c51502bea0a304009a7011
tw1ttt3r <tw1ttt3r@gmail.com>
2020-01-27 22:37:00 -0600

    chore: added route to visualizate listsong component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+import { ListsongsComponent } from './components/listsongs/listsongs.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  {path:'', component: ListsongsComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],

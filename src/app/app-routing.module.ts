1b5a002c7df36b5c67a703b7cce9b1f83a551125
tw1ttt3r <tw1ttt3r@gmail.com>
2020-01-27 22:42:22 -0600

    chore: added route /add

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 import { ListsongsComponent } from './components/listsongs/listsongs.component';
+import { AddsongComponent } from './components/addsong/addsong.component';
 
 
 const routes: Routes = [
-  {path:'', component: ListsongsComponent}
+  {path:'', component: ListsongsComponent},
+  {path:'add', component: AddsongComponent},
 ];
 
 @NgModule({

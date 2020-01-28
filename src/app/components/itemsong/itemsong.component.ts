f62c3773d28d4dba49a53cbfded9a7151336ea9e
tw1ttt3r <tw1ttt3r@gmail.com>
2020-01-28 01:18:37 -0600

    feat: added inputs to itemsong component

--- src/app/components/itemsong/itemsong.component.ts
+++ src/app/components/itemsong/itemsong.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-itemsong',
@@ -8,6 +8,14 @@ import { Component, OnInit } from '@angular/core';
 export class ItemsongComponent implements OnInit {
 
   constructor() { }
+
+  @Input() nameCancion: string;
+  
+  @Input() nameArtista: string;
+  
+  @Input() nameAlbum: string;
+  
+  @Input() anio: string;
 
   ngOnInit() {
   }

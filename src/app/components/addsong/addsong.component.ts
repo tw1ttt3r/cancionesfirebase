613771dfb87aaaed9956b6ffe03db6db934e01d7
tw1ttt3r <tw1ttt3r@gmail.com>
2020-01-28 01:43:34 -0600

    feat: generated logic to send data to service

--- src/app/components/addsong/addsong.component.ts
+++ src/app/components/addsong/addsong.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
+import { DatabaseFirebaseService } from 'src/app/services/database-firebase.service';
+import itemsong from 'src/app/interfaces/itemsong';
 
 @Component({
   selector: 'app-addsong',
@@ -7,9 +9,38 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AddsongComponent implements OnInit {
 
-  constructor() { }
+  constructor(private db: DatabaseFirebaseService) { }
+
+  @ViewChild('nameSong', {static: false}) nameSong: ElementRef;
+  @ViewChild('nameArtista', {static: false}) nameArtista: ElementRef;
+  @ViewChild('nameAlbum', {static: false}) nameAlbum: ElementRef;
+  @ViewChild('anio', {static: false}) anio: ElementRef;
+
+  newSong: itemsong = {album: '', anio: 0, name: '', artista: ''};
 
   ngOnInit() {
   }
 
+  addItem() {
+    if (this.nameSong.nativeElement.value === '' || this.nameArtista.nativeElement.value === ''
+     || this.nameAlbum.nativeElement.value === '' || this.anio.nativeElement.value === '') {
+       alert('Todos los campos son obligtorios')
+     } else {
+        this.newSong.album = this.nameAlbum.nativeElement.value;
+        this.newSong.anio = this.anio.nativeElement.value;
+        this.newSong.name = this.nameSong.nativeElement.value;
+        this.newSong.artista = this.nameArtista.nativeElement.value;
+        let guardada = this.db.addSong( this.newSong );
+
+        if (guardada === true) {
+          alert('Guardada Correctamente')
+          this.nameAlbum.nativeElement.value = '';
+          this.nameSong.nativeElement.value = '';
+          this.nameArtista.nativeElement.value = '';
+          this.anio.nativeElement.value = '';
+        } else {
+          console.log(guardada);
+        } 
+     }
+  }
 }
